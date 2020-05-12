67b07973c565912f8ac65df393c6fd15b9d14860
Joshua Fernelius <joshfern24@Joshuas-MacBook-Pro.local>
2020-05-12 10:12:35 -0600

    A lot of unnecessary code is removed

--- js/main.js
+++ js/main.js
@@ -15,40 +15,41 @@ let c = 0;
 let num1to10 = 1;
 
 /*----- cached element references -----*/ 
-/*----- event listeners -----*/ 
-document.getElementById('gameBoard').addEventListener('click', handleClick);
-document.querySelector('button').addEventListener('click', resetBtn)
 let gameBoardEl = document.getElementById('gameBoard');
 
 
-init();
+/*----- event listeners -----*/ 
 $('#gameBoard').one('click', function(e) {
     placeMines();
     render();
     interval = setInterval(formatTime, 1000);
 })
 
-// if(interval === null) {
-//     document.getElementById('gameBoard').addEventListener('click', function() {
-//         placeMines();
-//         interval = setInterval(formatTime, 1000);
-//     });
-// }
+document.getElementById('gameBoard').addEventListener('click', handleClick);
+document.getElementById('gameBoard').addEventListener('dblclick', handleDblClick);
 
+document.querySelector('button').addEventListener('click', resetBtn);
 
 /*----- functions -----*/
 
+init();
+
 function init() {
+    //Create the divs/the board dynamically
     for(let i = 0; i < 9; i++) {
         for(let j = 0; j < 9; j++) {
             let newDiv = document.createElement('div');
             newDiv.id = `c${j}r${i}`;
             newDiv.style.border = '1px solid';
-            newDiv.style.fontSize = '28px';
+            newDiv.style.borderTopColor = 'white';
+            newDiv.style.borderLeftColor = 'white';
+            newDiv.style.fontSize = '24px';
+            newDiv.style.fontStlye = 'cursive'
             gameBoardEl.appendChild(newDiv);
 
         }
     }
+    //Initializes the board array to 0
     board = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -75,6 +76,7 @@ function render() {
 }
 
 function placeMines() {
+    //Randomly places 10 mines throughout the board
     while(num1to10 <= 10) {
         r = Math.floor(Math.random() * 9);
         c = Math.floor(Math.random() * 9);
@@ -89,6 +91,8 @@ function placeMines() {
 }
 
 function asignNumbers(c, r) {
+    //Asigns the numbers surrounding mines
+    //All end cases are checked
     if((c===8)&&(r===8)){
         if(board[c-1][r-1] !== -1) board[c-1][r-1]++;
         if(board[c-1][r] !== -1) board[c-1][r]++;
@@ -106,7 +110,7 @@ function asignNumbers(c, r) {
     }
     else if((c===8)&&(r===0)){
         if(board[c-1][r] !== -1) board[c-1][r]++;
-        if(board[c-1][r] !== -1) board[c-1][r]++;
+        if(board[c-1][r+1] !== -1) board[c-1][r+1]++;
         if(board[c][r+1] !== -1) board[c][r+1]++;
     }
     else if(c===8){
@@ -147,57 +151,58 @@ function asignNumbers(c, r) {
         if(board[c+1][r] !== -1) board[c+1][r]++;
         if(board[c+1][r+1] !== -1) board[c+1][r+1]++;
     }
-    // for(let i = -1; i <= 1; i++) {
-    //     if(board[c + i][r] !== -1) board[c + 1][r]++;
-
-    // }
 }
 
 function revealBoard(c, r) {
     //if(board[c][r] === -1) alert('You lose');
-    if(board[c][r] === 0) {
-        revealEmpty(c, r);
-        revealBoard(c-1, r-1);
-        revealBoard(c-1, r);
-        revealBoard(c-1, r+1);
-        revealBoard(c, r-1);
-        revealBoard(c, r1);
-        revealBoard(c, r+1);
-        revealBoard(c+1, r-1);
-        revealBoard(c+1, r1);
-        revealBoard(c+1, r+1);
-
-    }
-    if(board[c][r] === 1) {
-        document.getElementById(`c${c}r${r}`).innerText = board[c][r];
-    }
-    if(board[c][r] === 2) {
-        document.getElementById(`c${c}r${r}`).innerText = board[c][r];
-    }
-    if(board[c][r] === 3) {
-        document.getElementById(`c${c}r${r}`).innerText = board[c][r];
-    }
-    
-}
-
-function revealEmpty(c, r) {
-    document.getElementById(`c${c}r${r}`).style.backgroundColor = 'black';
-    document.getElementById(`c${c}r${r}`).style.borderColor = '#cceef6';
+    if(board[c][r] === -1) {
+        document.getElementById(`c${c}r${r}`).innerText = '@';
+    } else {
+        document.getElementById(`c${c}r${r}`).style.backgroundColor = '#c9c1ad';
+        document.getElementById(`c${c}r${r}`).style.borderBottomColor = 'black';
+        document.getElementById(`c${c}r${r}`).style.borderRIghtColor = 'black';
+        document.getElementById(`c${c}r${r}`).style.borderTopColor = 'white';
+        document.getElementById(`c${c}r${r}`).style.borderLeftColor = 'white';
+        if(board[c][r] === 0) {
+        
+        } else {
+            document.getElementById(`c${c}r${r}`).style.backgroundColor = '#c9c1ad';
+            if(board[c][r] === 1) {
+                document.getElementById(`c${c}r${r}`).style.color = 'red';
+            }
+            if(board[c][r] === 2) {
+                document.getElementById(`c${c}r${r}`).style.color = 'blue';
+            }
+            if(board[c][r] === 3) {
+                document.getElementById(`c${c}r${r}`).style.color = 'green';
+            }
+            if(board[c][r] === 4){
+                document.getElementById(`c${c}r${r}`).style.color = 'purple';
+            }
+            document.getElementById(`c${c}r${r}`).style.borderRightColor = 'black';
+            document.getElementById(`c${c}r${r}`).innerText = board[c][r];
+        }
+    }   
 }
 
 function handleClick(e) {
     var col = (e.target.id).charAt(1);
     var row = (e.target.id).charAt(3);
-    if(e.target.innerText != '*'){
-        document.getElementById('minesLeft').innerText = `${parseInt(document.getElementById('minesLeft').innerText) - 1}`
-    }
     revealBoard(col, row);
+}
+
+function handleDblClick(e) {
+    e.target.innerText = '*';
+    document.getElementById('minesLeft').innerText = `${parseInt(document.getElementById('minesLeft').innerText) - 1}`;
 }
 
 function resetBtn() {
     for(let i = 0; i < 9; i++) {
         for(let j = 0; j < 9; j++){
+            board[i][j] = 0;
             document.getElementById(`c${i}r${j}`).innerText = '';
+            document.getElementById(`c${i}r${j}`).style.backgroundColor = '#918c7e';
+            document.getElementById(`c${i}r${j}`).style.borderColor = 'black';
         }
     }
     document.getElementById('minesLeft').innerText = '10';
