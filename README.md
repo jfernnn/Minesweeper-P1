82fb24b4ae7ec8f7fae992d4fde15963c1c8220e
Joshua Fernelius <joshfern24@Joshuas-MacBook-Pro.local>
2020-05-14 16:31:34 -0600

    CSS looks and works well

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ hidden on the board. This is accomplished using the numbers
 in the spaces around each mine, which describes how many mines
 are in the surrounding spaces. There are three dificulties;
 easy, medium, and hard. Growing up, minesweeper was one of the
-only games I had to play
+only games I had to play and it has always stuck with me as one of my favorites. 
 
 ## Screenshot
 
@@ -22,11 +22,11 @@ only games I had to play
 
 ## Getting Started
 
-To play this game, click [here](https://jfernnn.github.io/Minesweeper-P1/)
+To try out Minesweeper, click [here](https://jfernnn.github.io/Minesweeper-P1/)
 
 ## Next Steps
 
-1. Add different difficulties
+1. Add a third difficulty that is a rectangle
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 /*----- constants -----*/ 
 const boardSize = {
-    's': [9, 9],
-    'm': [16, 16],
-    'l': [16, 30],
+    's': 9,
+    'l': 16
 }
 
 const numMines = {
     's': 10,
-    'm': 40,
-    'l': 99
+    'l': 40,
 }
 
 /*----- app's state (variables) -----*/ 
@@ -22,22 +20,22 @@ let counter = 0;
 
 /*----- cached element references -----*/ 
 let gameBoardEl = document.getElementById('gameBoard');
-
+let h3El = document.querySelector('h3');
 
 /*----- event listeners -----*/ 
 document.getElementById('gameBoard').addEventListener('click', handleClick);
 document.getElementById('gameBoard').addEventListener('contextmenu', handler);
 document.getElementById('buttons').addEventListener('click', init)
-
-document.querySelector('button').addEventListener('click', resetBtn);
+document.getElementById('reset').addEventListener('click', resetBtn);
 
 /*----- functions -----*/
 
 
 function init(e) {
     //Create the divs/the board dynamically
+    gameBoardEl.innerHTML = '';
     size = e.target.id;
-    //let small = document.createElement('button');
+
     document.getElementById('buttons').style.display = 'none';
     gameBoardEl.style.backgroundColor = '#918c7e';
     gameBoardEl.style.border = '2px solid black';
@@ -48,26 +46,24 @@ function init(e) {
         gameBoardEl.style.width = '246px';
         gameBoardEl.style.gridTemplateColumns = 'repeat(9, 27px)';
         gameBoardEl.style.gridTemplateRows = 'repeat(9, 27px)';
+        h3El.innerText = '..you got it';
+        h3El.style.marginLeft = '35px';
     }
-    if(size === 'm'){
+    if(size === 'l'){
+        gameBoardEl.style.marginLeft = '-90px';
         gameBoardEl.style.width = '435px';
         gameBoardEl.style.gridTemplateColumns = 'repeat(16, 27px)';
         gameBoardEl.style.gridTemplateRows = 'repeat(16, 27px)';
+        h3El.style.marginLeft = '-20px';
+        h3El.innerText = `...you're gonna need it`
     }
-    if(size === 'l'){
-        gameBoardEl.style.width = '814px';
-        gameBoardEl.style.gridTemplateColumns = 'repeat(30, 27px)';
-        gameBoardEl.style.gridTemplateRows = 'repeat(16, 27px)';
-    }
-    gameBoardEl.innerHTML = '';
 
-    document.querySelector('h3').style.marginLeft = '68px';
-    document.querySelector('h3').innerText = 'Good Luck...';
+    h3El.style.marginLeft = '68px';
     document.getElementById('minesLeft').innerText = numMines[size];
     document.getElementById('timer').innerText = `00:00`;
 
-    for(let i = 0; i < boardSize[size][0]; i++) {
-        for(let j = 0; j < boardSize[size][1]; j++) {
+    for(let i = 0; i < boardSize[size]; i++) {
+        for(let j = 0; j < boardSize[size]; j++) {
             let newDiv = document.createElement('div');
             newDiv.id = `c${j}r${i}`;
             newDiv.style.border = '1px solid';
@@ -81,12 +77,12 @@ function init(e) {
     }
     //Initializes the board with objects
     board = [];
-    for(let i = 0; i < boardSize[size][0]; i++) {
+    for(let i = 0; i < boardSize[size]; i++) {
         board[i] = [];
     }
-    for(let i = 0; i < boardSize[size][0]; i++){
-        for(let j = 0; j < boardSize[size][1]; j++) {
-            board[j][i] = {
+    for(let i = 0; i < boardSize[size]; i++){
+        for(let j = 0; j < boardSize[size]; j++) {
+            board[i][j] = {
                 pos: `c${j}r${i}`,
                 isMine: false,
                 revealed: false,
@@ -96,13 +92,15 @@ function init(e) {
             }
         }
     }
+
 }
 
 
 function render(c, r) {
-
+    //Grabs the div
     let squareEl = document.getElementById(`c${c}r${r}`);
 
+    //Renders the squares with bombs if they have mines
     if(board[c][r].isMine === true) {
         if(board[c][r].hasFlag) {
             document.getElementById(`c${c}r${r}`).removeChild(document.getElementById(`c${c}r${r}img`));
@@ -116,11 +114,12 @@ function render(c, r) {
         board[c][r].revealed = true;
 
         squareEl.style.backgroundColor = '#c9c1ad';
+        //Reveals the nearby squares if the square selected is empty
         if(board[c][r].isEmpty) {
             revealNearbyEmpties(c, r);
         }
         else {
-            
+            //Styles the numbers and squares after they are revealed
             squareEl.style.backgroundColor = '#c9c1ad';
             squareEl.style.borderRightColor = 'black';
             squareEl.style.borderBottomColor = 'black';
@@ -146,38 +145,40 @@ function render(c, r) {
     } 
 }
 
+//If all the squares that are not mines are revealed, the player wins
 function checkWinner(){
-    for(let i = 0; i < boardSize[size][1]; i++){
-        for(let j = 0; j < boardSize[size][0]; j++){
+    for(let i = 0; i < boardSize[size]; i++){
+        for(let j = 0; j < boardSize[size]; j++){
             if (!(board[i][j].isMine)&&!(board[i][j].revealed)) {
                 return;
             }
         }
     }
     clearInterval(interval);
-    document.querySelector('h3').innerText = 'Look at you';
+    h3El.style.marginLeft = '60px';
+    h3El.innerText = 'You Won!!';
 }
 
 
 function placeMines(e) {
-    //Randomly places 10 mines throughout the board
-    console.log('asdfadf');
+    //Randomly places mines throughout the board
     let idOfEl = e.target.id;
     
-    const ind2 = (e.target.id).indexOf('r');
-    console.log(ind2);
-    var col = parseInt((e.target.id).substring(1, ind2));
-    console.log(col);
-    var row = parseInt((e.target.id).substring(ind2+1, (e.target.id).length));
-    console.log(row);
+
+    const ind2 = (idOfEl).indexOf('r');
+    var col = parseInt((idOfEl).substring(1, ind2));
+    var row = parseInt((idOfEl).substring(ind2+1, (idOfEl).length));
+
     let mines1to10 = 1;
     let r = 0;
     let c = 0;
     while(mines1to10 <= numMines[size]) {
-        r = Math.floor(Math.random() * boardSize[size][0]);
-        c = Math.floor(Math.random() * boardSize[size][1]);
+        //finds 2 random numbers to find a random board slot
+        r = Math.floor(Math.random() * boardSize[size]);
+        c = Math.floor(Math.random() * boardSize[size]);
+
+        //puts a mine on the random spot and calls asignNumbers
         if((!board[c][r].isMine)&&(board[col][row] !== board[c][r])) {
-            
             board[c][r].isMine = true;
             asignNumbers(c, r);
             mines1to10++;
@@ -187,8 +188,11 @@ function placeMines(e) {
 }
 
 function findEmptySpaces() {
-    for(let i = 0; i < boardSize[size][1]; i++){
-        for(let j = 0; j < boardSize[size][0]; j++){
+    //Finds all empty spaces on the board and marks them
+    //All end cases are checked
+
+    for(let i = 0; i < boardSize[size]; i++){
+        for(let j = 0; j < boardSize[size]; j++){
             if(board[i][j].surroundsMines === 0) {
                 if(board[i][j].isMine === false) {
                     board[i][j].isEmpty = true;
@@ -204,8 +208,8 @@ function asignNumbers(c, r) {
 
     for(var i = -1; i <= 1; i++) {
         for(var j = -1; j <= 1; j++) {
-            if(!(c + i < 0)&&!(c + i > boardSize[size][1] - 1)){
-                if(!(r + j < 0)&&!(r + j > boardSize[size][0] - 1)){
+            if(!(c + i < 0)&&!(c + i > boardSize[size] - 1)){
+                if(!(r + j < 0)&&!(r + j > boardSize[size] - 1)){
                     if((c+i!==0)||(r+j!==0)){
                         board[c+i][j+r].surroundsMines++;
                     }
@@ -217,14 +221,17 @@ function asignNumbers(c, r) {
 
 
 function revealNearbyEmpties(c, r) {
+    //Marks all spaces on the board that are empty
+    //Checks all end cases
+
     for(var i = -1; i <= 1; i++) {
         for(var j = -1; j <= 1; j++) {
             var zz = 0;
             var yy = 0;
             zz = parseInt(c) + i;
             yy = parseInt(r) + j;
-            if(!(zz < 0)&&!(zz > boardSize[size][1] - 1)){
-                if(!(yy < 0)&&!(yy > boardSize[size][0] - 1)){
+            if(!(zz < 0)&&!(zz > boardSize[size] - 1)){
+                if(!(yy < 0)&&!(yy > boardSize[size] - 1)){
                     if((zz!==c)||(yy!==r)){
                         if(board[zz][yy].revealed === false){
                             if(board[zz][yy].isMine === false){
@@ -251,8 +258,10 @@ function handler(e) {
 }
 
 function handleClick(e) {
+    //If this is the first click on the board by the player
     if(newGame) {
         newGame = false;
+        //Places mines and finds all empty spaces
         placeMines(e);
         findEmptySpaces();
         interval = setInterval(formatTime, 1000);
@@ -261,21 +270,21 @@ function handleClick(e) {
     var col = parseInt((e.target.id).substring(1, ind2));
     var row = parseInt((e.target.id).substring(ind2+1, (e.target.id).length));
 
-    //if(e.target.innerText !== '*'){
-        if(board[col][row].isMine) {
-            for(let i = 0; i < boardSize[size][1]; i++){
-                for(let j = 0; j < boardSize[size][0]; j++){
-                    render(i, j);
-                }
+    if(!((board[col][row]).hasFlag)){
+    if(board[col][row].isMine) {
+        for(let i = 0; i < boardSize[size]; i++){
+            for(let j = 0; j < boardSize[size]; j++){
+                render(i, j);
             }
-            clearInterval(interval);
-            document.querySelector('h3').style.marginLeft = '-20px';
-            document.querySelector('h3').innerText = 'Did you expect any better?';     
-        } else {
-        render(col, row);
-        checkWinner();
         }
-    //}
+        clearInterval(interval);
+        h3El.style.marginLeft = '-20px';
+        h3El.innerText = 'Did you expect any better?';     
+    } else {
+    render(col, row);
+    checkWinner();
+    }
+    }
 }
 
 function handleRghtClick(e) {
@@ -297,13 +306,11 @@ function handleRghtClick(e) {
             board[col][row].hasFlag = true;
             
             squareEl.append(flagImg);
-            //squareEl.innerText = '*';
 
             document.getElementById('minesLeft').innerText = `${parseInt(document.getElementById('minesLeft').innerText) - 1}`;
         } else  {
             board[col][row].hasFlag = false
             squareEl.removeChild(document.getElementById(`c${col}r${row}img`));
-            //squareEl.innerText = '';
 
             document.getElementById('minesLeft').innerText = `${parseInt(document.getElementById('minesLeft').innerText) + 1}`;
         }
@@ -317,6 +324,8 @@ function resetBtn() {
     gameBoardEl.innerHTML = '';
     $('#gameBoard').removeAttr('style');
     document.getElementById('buttons').style.display = 'block';
+    h3El.innerText = 'Good Luck...';
+    h3El.style.marginLeft = '68px';
 
     newGame = true;
 
@@ -325,7 +334,7 @@ function resetBtn() {
     // restart properly
 }
 
-
+//Formats the timer to display in the 00:00 format
 function formatTime() {
     seconds++;
     const mins = Math.floor(seconds / 60).toString().padStart(2, '0');
